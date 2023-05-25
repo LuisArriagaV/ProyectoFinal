4d8f987470e2e9323b4a6633f144ad17d771ef51
LuisArriagaV <wuichva280998@outlook.com>
2023-05-25 16:35:20 -0600

    25-05-23

---
+++ primeravez.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+def P_first_passage(P, x, y, n):
+    """
+    Calcula la probabilidad de primer paso de x a y en n pasos usando la fórmula de renovación.
+    """
+    P_n = np.linalg.matrix_power(P, n)  # Matriz de transición a la n
+    P_first = P_n[x][y]  # Probabilidad de primer paso directa
+    
+    for m in range(1, n):
+        # Suma de las probabilidades de primer paso anteriores
+        sum_P_first = sum([P_first_passage(P, x, y, m) * np.linalg.matrix_power(P, n-m)[y][y] for m in range(1, n)])
+    
+    P_first = P_first - sum_P_first  # Probabilidad de primer paso utilizando la fórmula de renovación
+    
+    return P_first
+
+# Definir la matriz de transición P
+P = np.array([[0.3, 0.5, 0.2],
+              [0.1, 0.6, 0.3],
+              [0.2, 0.4, 0.4]])
+
+# Definir el estado inicial x
+x = 0
+
+# Definir el estado objetivo y
+y = 2
+
+# Definir el número de pasos n
+n = 5
+
+# Calcular la probabilidad de primer paso utilizando la fórmula de renovación
+P_first = P_first_passage(P, x, y, n)
+
+print("La probabilidad de primer paso P_x(T_y =", n, ") es:", P_first)
--- suma.py
+++ suma.py
@@ -1,12 +1,35 @@
+import numpy as np
+
+
 def suma(x,y,n):
     m = n-1
     x = EstadoInicial
     y = EstadoFinal
 
-    if m = 0:
+    if m == 0:
         return 0
     
-    if m = 1:
+    if m == 1:
         return  np.linalg.matrix_power(matriz,n)[x,y]
     
     return suma
+
+EstadoInicial = 1
+EstadoFinal = 5
+n=6
+
+Estados = int(input("Indica el número de estados : "))
+
+#Matrix generation
+matriz = []
+for i in range(Estados):
+    matriz.append([])
+    for j in range(Estados):
+        valor = float(input("Dame el valor del estado {}, {}:  ".format(i , j)))
+        
+
+        matriz[i].append(valor)
+        print()
+
+print("La suma es : ")
+print(suma(EstadoInicial,EstadoFinal,n))
